69c16f24911c8ed1a3ef7204dac0d3c9bdd12f02
DavidSheltonSF <david.faria012@gmail.com>
2026-02-26 23:42:46 -0300

    feat(scripts): add script to controll the carousel

---
+++ src/scripts/carousel.js
@@ -0,0 +1,66 @@
+const carouselRightButton = document.querySelector('.carousel__right-button');
+const carouselLeftButton = document.querySelector('.carousel__left-button');
+const carouselItemsContainer = document.querySelector('.carousel__items-list');
+
+const itemsQuantity = 5;
+const visibleItems = 3;
+const itemsLength = 400;
+const itemsGap = 24;
+let itemsAtLeft = 0;
+let itemsAtRight = itemsQuantity - visibleItems;
+const step = itemsLength + (itemsGap * visibleItems);
+let position = 0;
+
+
+function moveNext(){
+  if(itemsAtRight === 0) {
+    return; 
+  };
+  position -= step;
+  itemsAtLeft ++;
+  itemsAtRight --
+}
+
+function moveBack(){
+  if(itemsAtLeft === 0) {
+    return
+  };
+  position += step;
+  itemsAtLeft --;
+  itemsAtRight ++;
+}
+
+function checkButtons() {
+  if(itemsAtRight === 0){
+    carouselRightButton.classList.remove('active-button');
+    carouselRightButton.classList.add('disabled-button');
+  }
+
+   if(itemsAtRight > 0){
+    carouselRightButton.classList.add('active-button');
+    carouselRightButton.classList.remove('disabled-button');
+  }
+
+   if(itemsAtLeft === 0){
+    carouselLeftButton.classList.remove('active-button');
+    carouselLeftButton.classList.add('disabled-button');
+  }
+
+   if(itemsAtLeft > 0){
+    carouselLeftButton.classList.add('active-button');
+    carouselLeftButton.classList.remove('disabled-button');
+  }
+}
+
+carouselRightButton.addEventListener('click', () => {
+  moveNext()
+  checkButtons()
+  carouselItemsContainer.style.transform  = `translateX(${position}px)`
+})
+
+
+carouselLeftButton.addEventListener('click', () => {
+  moveBack();
+  checkButtons();
+  carouselItemsContainer.style.transform = `translateX(${position}px)`
+})
