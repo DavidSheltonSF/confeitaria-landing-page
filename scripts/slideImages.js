432aa3f70824b327477014d9c948b4201a6788cb
DavidSheltonSF <david.faria012@gmail.com>
2026-03-02 20:50:54 -0300

    chore: use webp images instead of png or webp for better performance

--- scripts/slideImages.js
+++ scripts/slideImages.js
@@ -1,24 +1,24 @@
 export const slideImages = [
   {
-    path: '../public/bolo-de-maracuja.png',
+    path: '../public/bolo-de-maracuja.webp',
     alt: 'Bolo de maracuja com chantili e pedaços de maracuja'
   },
    {
-    path: '../public/bolo-de-chocolate.jpg',
+    path: '../public/bolo-de-chocolate.webp',
     alt: 'Bolo de chocolate redondo e fatiado'
   },
 
    {
-    path: '../public/bolo-de-pote.png',
+    path: '../public/bolo-de-pote.webp',
     alt: 'Bolo de pode com chocolate preto, chocolate branco e pedaços de morango'
   },
    {
-    path: '../public/bolo-fatias.jpg',
+    path: '../public/bolo-fatias.webp',
     alt: 'Cinco fatias de bolo uma do lado da outra'
 
   },
    {
-    path: '../public/bolo-de-morango.jpg',
+    path: '../public/bolo-de-morango.webp',
     alt: "Dois bolos de morango com pedaços de morango"
   }
 ]
