27311f01d962714517b7c2239167253b7e50e08c
DavidSheltonSF <david.faria012@gmail.com>
2026-03-03 14:52:47 -0300

    chore: add lazy loading to the slider images and to the client's avatar images

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,7 +5,7 @@ const sliderNav = document.querySelector('.slider-nav')
 document.addEventListener('DOMContentLoaded', () => {
   slideImages.map((image, index) => {
     const sliderItem = `
-      <img id="slide-${index}" src=${image.path} alt=${image.alt}>
+      <img id="slide-${index}" src=${image.path} alt=${image.alt} loading="lazy">
     `
     const navButton = `
       <a href="#slide-${index}"></a>
