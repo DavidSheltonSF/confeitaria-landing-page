f9c45695c75ecbe86f1a4e96a72a2df6c8f2d499
DavidSheltonSF <david.faria012@gmail.com>
2026-03-02 22:04:24 -0300

    chore(slideImages - scripts): use the resized slide images for better performance

--- scripts/slideImages.js
+++ scripts/slideImages.js
@@ -1,19 +1,20 @@
 export const slideImages = [
   {
-    path: '../public/slides/bolo-de-maracuja.webp',
+    path: '../public/slides/bolo-de-maracuja-1144x761.webp',
     alt: 'Bolo de maracuja com chantili e pedaços de maracuja'
   },
    {
-    path: '../public/slides/bolo-de-chocolate.webp',
+    path: '../public/slides/bolo-de-chocolate-1144x761.webp',
     alt: 'Bolo de chocolate redondo e fatiado'
   },
 
    {
-    path: '../public/slides/bolo-de-pote.webp',
+    path: '../public/slides/bolo-de-pote-1144x761.webp',
     alt: 'Bolo de pode com chocolate preto, chocolate branco e pedaços de morango'
   },
    {
-    path: '../public/slides/bolo-fatias.webp',
+    path: '../public/slides/bolo-fatias-1144x761.webp',
     alt: 'Cinco fatias de bolo uma do lado da outra'
   },
 ]
+
