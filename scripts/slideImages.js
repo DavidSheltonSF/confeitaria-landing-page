5a9b2b86bee140ffbc429051fd41f8bfdd2db270
DavidSheltonSF <david.faria012@gmail.com>
2026-03-02 21:03:52 -0300

    chore(public): move all slides images to public/slides and update .
    Also, remove the last item from slideImages

--- scripts/slideImages.js
+++ scripts/slideImages.js
@@ -1,24 +1,19 @@
 export const slideImages = [
   {
-    path: '../public/bolo-de-maracuja.webp',
+    path: '../public/slides/bolo-de-maracuja.webp',
     alt: 'Bolo de maracuja com chantili e pedaços de maracuja'
   },
    {
-    path: '../public/bolo-de-chocolate.webp',
+    path: '../public/slides/bolo-de-chocolate.webp',
     alt: 'Bolo de chocolate redondo e fatiado'
   },
 
    {
-    path: '../public/bolo-de-pote.webp',
+    path: '../public/slides/bolo-de-pote.webp',
     alt: 'Bolo de pode com chocolate preto, chocolate branco e pedaços de morango'
   },
    {
-    path: '../public/bolo-fatias.webp',
+    path: '../public/slides/bolo-fatias.webp',
     alt: 'Cinco fatias de bolo uma do lado da outra'
-
   },
-   {
-    path: '../public/bolo-de-morango.webp',
-    alt: "Dois bolos de morango com pedaços de morango"
-  }
 ]
