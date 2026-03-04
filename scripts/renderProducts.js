f487f124428cea6e664f8f9003c30fb7c8e85e2b
DavidSheltonSF <david.faria012@gmail.com>
2026-03-04 16:12:54 -0300

    refactor(renderProsuct): add search feature to the products section

--- scripts/renderProducts.js
+++ scripts/renderProducts.js
@@ -1,9 +1,15 @@
-import { products } from "./products.js";
+import { products } from './products.js';
 
-const productsList = document.querySelector('.products')
+const productsList = document.querySelector('.products');
+const searchBarInput = document.querySelector('.search-bar__input');
 
-function renderProducts() {
-  products.map((product) => {
+function clearProducts() {
+  productsList.innerHTML = '';
+}
+
+function renderProducts(prod) {
+  clearProducts()
+  prod.map((product) => {
     const productCard = `
     <article class="product">
       <img class="product__image" src="${product.imageUrl}" alt="">
@@ -13,16 +19,27 @@ function renderProducts() {
         <p class="product__price">R$${product.price}</p>
         </div>
         <button class="product__button button-primary">
-          <a href="https://wa.me/5521969470527?text=${product.whatsAppText.replace(' ', '%20')}">
+          <a href="https://wa.me/5521969470527?text=${product.whatsAppText.replace(
+            ' ',
+            '%20',
+          )}">
             PEDIR AGORA
           </a>  
         </button>
       </main>
     </article>
-    `
+    `;
 
     productsList.innerHTML += productCard;
   });
 }
 
-document.addEventListener('DOMContentLoaded', renderProducts)
+document.addEventListener('DOMContentLoaded', () => {
+  renderProducts(products)
+});
+
+searchBarInput.addEventListener('input', (e) => {
+  const filteredProducts = products.filter((product) => product.name.startsWith(e.target.value));
+
+  renderProducts(filteredProducts)
+});
