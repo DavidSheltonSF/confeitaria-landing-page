f15bfb61bd45277987fb39c04c850e65a58bf9e9
DavidSheltonSF <david.faria012@gmail.com>
2026-03-04 15:50:07 -0300

    feat(scripts): add products.js list of products

---
+++ scripts/products.js
@@ -0,0 +1,49 @@
+export const products = [
+  {
+    name: 'Bem casado',
+    imageUrl: './public/products/bem-casados.png',
+    price: 10.2,
+    whatsAppText: 'Olá, gostaria de pedir um Bolo de castanhas',
+  },
+
+  {
+    name: 'Brigadeiro gourmet',
+    imageUrl: './public/products/brigadeiros-gourmet.png',
+    price: 6.5,
+    whatsAppText: 'Olá, gostaria de pedir um Brigadeiro gourmet',
+  },
+  {
+    name: 'Bolo de pote',
+    imageUrl: './public/products/bolo-de-pote.png',
+    price: 16.2,
+    whatsAppText: 'Olá, gostaria de pedir um Bolo de castanhas',
+  },
+
+  {
+    name: 'Cupcake decorado',
+    imageUrl: './public/products/cupcakes-decorados.png',
+    price: 8.9,
+    whatsAppText: 'Olá, gostaria de pedir um Cupcake decorado',
+  },
+
+  {
+    name: 'Bolo de castanhas',
+    imageUrl: './public/products/bolo-de-castanhas.png',
+    price: 10.2,
+    whatsAppText: 'Olá, gostaria de pedir um Bolo de castanhas',
+  },
+
+  {
+    name: 'Torta de limão',
+    imageUrl: './public/products/torta-de-limao.png',
+    price: 12.5,
+    whatsAppText: 'Olá, gostaria de pedir uma Torta de limão',
+  },
+
+  {
+    name: 'Pudim tradicional',
+    imageUrl: './public/products/pudim-tradicional.png',
+    price: 9.9,
+    whatsAppText: 'Olá, gostaria de pedir um Pudim tradicional',
+  },
+];
