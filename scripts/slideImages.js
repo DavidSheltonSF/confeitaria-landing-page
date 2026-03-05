98a6ae1cfc22cb7d7b7373e613903de9c8ae315c
DavidSheltonSF <david.faria012@gmail.com>
2026-03-04 21:43:37 -0300

    fix(slideImages.js): fix the path to the images

--- scripts/slideImages.js
+++ scripts/slideImages.js
@@ -2,24 +2,24 @@ export const slideImages = [
   {
     path: '../public/slides/bolo-de-maracuja-1144x761.webp',
     alt: 'Bolo de maracuja com chantili e pedaços de maracuja',
-    srcset: "./public/slides/bolo-de-maracuja-800x842.webp 720w, ../public/slides/bolo-de-maracuja-1144x761.webp 768w"
+    srcset: "./public/slides/bolo-de-maracuja-800x842.webp 720w, ./public/slides/bolo-de-maracuja-1144x761.webp 768w"
   },
    {
-    path: '../public/slides/bolo-de-chocolate-1144x761.webp',
+    path: './public/slides/bolo-de-chocolate-1144x761.webp',
     alt: 'Bolo de chocolate redondo e fatiado',
-    srcset: "./public/slides/bolo-de-chocolate-800x842.webp 720w, ../public/slides/bolo-de-chocolate-1144x761.webp 768w"
+    srcset: "./public/slides/bolo-de-chocolate-800x842.webp 720w, ./public/slides/bolo-de-chocolate-1144x761.webp 768w"
 
   },
 
    {
     path: './public/slides/bolo-de-pote-1144x761.webp',
     alt: 'Bolo de pode com chocolate preto, chocolate branco e pedaços de morango',
-    srcset: "../public/slides/bolo-de-pote-800x842.webp 720w, ../public/slides/bolo-de-pote-1144x761.webp 768w"
+    srcset: "./public/slides/bolo-de-pote-800x842.webp 720w, ./public/slides/bolo-de-pote-1144x761.webp 768w"
   },
    {
     path: './public/slides/bolo-fatias-1144x761.webp',
     alt: 'Cinco fatias de bolo uma do lado da outra',
-    srcset: "./public/slides/bolo-fatias-800x842.webp 720w, ../public/slides/bolo-fatias-1144x761.webp 768w"
+    srcset: "./public/slides/bolo-fatias-800x842.webp 720w, ./public/slides/bolo-fatias-1144x761.webp 768w"
   },
 ]
 
