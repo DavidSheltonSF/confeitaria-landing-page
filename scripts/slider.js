ec7ec76092307d8c7eacd22a9b1a1e3352f39afc
DavidSheltonSF <david.faria012@gmail.com>
2026-03-05 18:20:03 -0300

    perf: add decoding="async" to the clients avatars, to the sliders and to the background image in the Clients Testimonials Section

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,8 +5,8 @@ const sliderNav = document.querySelector('.slider-nav')
 document.addEventListener('DOMContentLoaded', () => {
   slideImages.map((image, index) => {
     const sliderItem = `
-      <img id="slide-${index}" src=${image.path} alt=${image.alt} loading="lazy" srcset="${image.srcset}" sizes="100%">
-    `
+      <img id="slide-${index}" src=${image.path} alt=${image.alt} loading="lazy" decoding="async" srcset="${image.srcset}" sizes="100%">
+    `;
     const navButton = `
       <a href="#slide-${index}"></a>
     `
